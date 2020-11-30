9b72bba96ff8bcad8bdc1ee07163f9e388079ab7
hadz333 <dinohadzic28@gmail.com>
2020-11-30 12:26:06 -0800

    trying with build included

---
+++ client/build/precache-manifest.2f972fdb8965d91d2f42a09a62af01f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51ce64700b1cfc903d689c44d744528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f27c6ab05b31a62cad0e",
+    "url": "/static/css/main.e3450431.chunk.css"
+  },
+  {
+    "revision": "d3539b93121e8f0e87fc",
+    "url": "/static/js/2.04101dea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.04101dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f27c6ab05b31a62cad0e",
+    "url": "/static/js/main.4fdb1658.chunk.js"
+  },
+  {
+    "revision": "88435c47b2e515691be3",
+    "url": "/static/js/runtime-main.f2beb8d7.js"
+  }
+]);
