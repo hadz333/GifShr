35e4c1424a0cee2a314f0de30ca1a91d108d6349
hadz333 <dinohadzic28@gmail.com>
2020-11-29 22:48:11 -0800

    more

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
+router.get('/', async (req, res) => {
+  res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'))
+})
+
 router.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
