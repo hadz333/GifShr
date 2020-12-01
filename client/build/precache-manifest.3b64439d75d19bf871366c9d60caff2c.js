1604a531506e835e4076b8c02c0067a814d2b8a9
hadz333 <dinohadzic28@gmail.com>
2020-12-01 15:06:11 -0800

    removed build from .gitignore again

---
+++ client/build/precache-manifest.3b64439d75d19bf871366c9d60caff2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc00ffd74f997712e35ccf3ed202e97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28ed3fa514a9edf73849",
+    "url": "/static/css/main.e3450431.chunk.css"
+  },
+  {
+    "revision": "d3539b93121e8f0e87fc",
+    "url": "/static/js/2.04101dea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.04101dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ed3fa514a9edf73849",
+    "url": "/static/js/main.66dcd2a4.chunk.js"
+  },
+  {
+    "revision": "88435c47b2e515691be3",
+    "url": "/static/js/runtime-main.f2beb8d7.js"
+  }
+]);
